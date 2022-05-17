9ba5544e2a6c8507c2c8825b9a807b6b45de9df3
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-05-17 00:46:02 -0300

    feat: rotas não utilizadas

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,12 +4,11 @@ var usuarioController = require("../controllers/usuarioController.js");
 // var uploadAvatar = require("../middlewares/multer.js");
 var authenticator = require('../middlewares/authenticator')
 
-router.get("/produto/cadastrar", authenticator, usuarioController.cadastroProduto);
+
 router.get("/perfil/editar", authenticator, usuarioController.telaEditarPerfil);
 router.put("/perfil/editar", authenticator, usuarioController.editarPerfil); //uploadAvatar
 router.get("/pagamento", authenticator, usuarioController.pagamento);
 router.get("/carrinho", authenticator, usuarioController.carrinho);
-router.get("/chat", authenticator, usuarioController.chat);
 router.get("/comprar", usuarioController.comprar);
 router.get("/trocarsenha", authenticator, usuarioController.trocarSenha);
 router.get("/endereco", usuarioController.endereco);
